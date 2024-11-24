b84700d2ac194267fff7392084d9709b8bc345e4
ATLAS129 <yanis20092525@gmail.com>
2024-11-24 18:26:10 +0200

    a little change

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -4,22 +4,18 @@ import { MdOutlineLightMode, MdDarkMode } from "react-icons/md";
 const ToggleButton = () => {
   const { theme, toggleTheme } = useTheme();
 
-  return (
-    <button className="">
-      {theme === "dark" ? (
-        <MdDarkMode
-          onClick={toggleTheme}
-          size={30}
-          className="cursor-pointer transition"
-        />
-      ) : (
-        <MdOutlineLightMode
-          onClick={toggleTheme}
-          size={30}
-          className="cursor-pointer transition"
-        />
-      )}
-    </button>
+  return theme === "dark" ? (
+    <MdDarkMode
+      onClick={toggleTheme}
+      size={30}
+      className="cursor-pointer transition"
+    />
+  ) : (
+    <MdOutlineLightMode
+      onClick={toggleTheme}
+      size={30}
+      className="cursor-pointer transition"
+    />
   );
 };
 
